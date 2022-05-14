cb7f94e1630e506938fa82c91056b6cd056c9c0e
tiye <jiyinyiyong@gmail.com>
2022-05-14 19:19:35 +0800

    more apis and add test; tag 0.5.0-a1

--- tests/arithmetic_test.rs
+++ tests/arithmetic_test.rs
@@ -1,6 +1,6 @@
 extern crate quaternions;
 
-use quaternions::Quaternion;
+use quaternions::{q, qi, Quaternion};
 
 /// Tests
 
@@ -10,7 +10,44 @@ static PI: f32 = std::f32::consts::PI;
 
 #[test]
 fn test_arithmetic() {
-  assert!(Quaternion::new(1., 2., 3., 4.) + Quaternion::<f32>::new(5., 6., 7., 8.) == Quaternion::<f32>::new(6., 8., 10., 12.));
+  assert_eq!(qi::<f32>(1, 2, 3, 4) + qi(5, 6, 7, 8), qi(6, 8, 10, 12));
+  assert_eq!(qi::<f32>(1, 2, 3, 4) - qi(5, 6, 7, 8), qi(-4, -4, -4, -4));
+  assert_eq!(qi::<f32>(1, 2, 3, 4).scale(1.5), q(1.5, 3., 4.5, 6.)); // float number comparing has problems
+
+  // div depends on order
+
+  assert_eq!(qi::<f32>(1, 2, 3, 4) * qi(5, 6, 7, 8), qi::<f32>(-60, 12, 30, 24));
+  assert_eq!(qi::<f64>(-60, 12, 30, 24) / qi(5, 6, 7, 8), qi::<f64>(1, 2, 3, 4));
+
+  assert_eq!(qi(5, 6, 7, 8) * qi::<f32>(1, 2, 3, 4), qi::<f32>(-60, 20, 14, 32));
+  assert_eq!(qi::<f32>(-60, 20, 14, 32) / qi::<f32>(1, 2, 3, 4), qi(5, 6, 7, 8));
+
+  assert_eq!(qi::<f32>(1, 2, 3, 4).conjugate(), qi::<f32>(1, -2, -3, -4));
+  assert_eq!(qi::<f32>(1, 2, 3, 4).square_length(), 30.);
+  assert_eq!(qi::<f32>(1, 2, 3, 4).length(), (30.0_f32).sqrt());
+
+  assert_eq!(qi::<f32>(1, 2, 3, 4).inverse() * qi(1, 2, 3, 4), qi::<f32>(1, 0, 0, 0));
+  assert_eq!(Quaternion::id(), qi::<f32>(1, 0, 0, 0));
+
+  assert_eq!(Quaternion::default(), qi::<f32>(0, 0, 0, 0));
+
+  assert_eq!(qi::<f32>(1, 2, 3, 4).dot(&qi::<f32>(5, 6, 7, 8)), 70.);
+}
+
+#[test]
+fn test_arithmetic_mut() {
+  let mut a = qi::<f32>(1, 2, 3, 4);
+  a += qi(5, 6, 7, 8);
+  assert_eq!(a, qi(6, 8, 10, 12));
+
+  let mut b = qi::<f32>(1, 2, 3, 4);
+  b *= qi(5, 6, 7, 8);
+  assert_eq!(b, qi::<f32>(-60, 12, 30, 24));
+
+  let mut c = qi::<f32>(1, 2, 3, 4);
+  let c2 = c.to_owned();
+  c.inverse_mut();
+  assert_eq!(c * c2, qi::<f32>(1, 0, 0, 0));
 }
 
 #[test]
